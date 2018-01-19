736743a084932e03ee41da9ef232e3e59c096849
LIniker Silva <contato@liniker.com.br>
2018-01-19 18:15:10 -0200

    correção de erros

--- src/app.ts
+++ src/app.ts
@@ -14,10 +14,11 @@ class App {
 
     private middleware(): void {
         this.express.use('/graphql', graphqlHTTP({
-            schema: schema,
-            graphiql: process.env.NODE_ENV === 'development'
-        });
-    });
+                schema: schema,
+                graphiql: process.env.NODE_ENV === 'development'
+            })
+        );
+    };
 }
 
 export default new App().express;
