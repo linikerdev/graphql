aea9a74fddc6be008e24ca312f71a6d46feaa3e9
LIniker Silva <contato@liniker.com.br>
2018-01-26 17:02:13 -0200

    Instalação do lodash

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,11 +1,24 @@
-
 import { makeExecutableSchema } from "graphql-tools";
+import { merge } from "lodash";
 
 import { Query } from './query';
 import { Mutation } from './mutation';
+
 import { userTypes } from "./resources/user/user.schema";
 import { postTypes } from "./resources/post/post.schema";
 import { commentTypes } from "./resources/comment/comment.schema";
+
+import { commentResolvers } from './resources/comment/comment.resolvers';
+import { userResolvers } from './resources/user/user.resolvers';
+import { postResolvers } from './resources/post/post.resolvers';
+
+const resolvers = merge(
+    commentResolvers,
+    postResolvers,
+    userResolvers
+);
+
+
 
 
 const SchemaDefinition = `  
